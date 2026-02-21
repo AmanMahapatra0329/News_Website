affa7255e92126fea51dc94cea8dfb43d5c2a66d
Am <code.c.909.c@gmail.com>
2026-02-21 19:58:35 +0530

    updated home page with mainpage.

--- article/models.py
+++ article/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
-class Article(models.Model):
-    name=models.CharField(max_length=50)
-    age=models.IntegerField()
-    vote=models.IntegerField()
-
-    def __str__(self):
-        return self.name
+class Herosection(models.Model):
+    title=models.CharField(max_length=1000)
+    description=models.TextField()
+    image=models.ImageField()
--- article/views.py
+++ article/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def HFpage(request):
-    return render(request,'article/HF.html')
+    return render(request,'article/mainpage.html')
