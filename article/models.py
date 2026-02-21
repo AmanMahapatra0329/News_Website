7fcaf8fe802b307281f4a31b2627d375ae059df3
Am <code.c.909.c@gmail.com>
2026-02-21 20:33:05 +0530

    admin authority

--- article/models.py
+++ article/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Herosection(models.Model):
     title=models.CharField(max_length=1000)
     description=models.TextField()
-    image=models.ImageField()
+    image=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
