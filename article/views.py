f8dd11cf50eea3d94d8868f8740a8f3cb934d058
Am <code.c.909.c@gmail.com>
2026-02-22 16:17:03 +0530

    news article cart added

--- article/views.py
+++ article/views.py
@@ -5,7 +5,7 @@ from .models import Herosection,breakingnews
 def homepage(request):
     hero=Herosection.objects.all()
     breaking=breakingnews.objects.all()
-    return render(request,'article/mainpage.html',{"hero":hero,"breaking":breaking})
+    return render(request,'article/newslist.html',{"hero":hero,"breaking":breaking})
 
 # def breakingnews_text(request):
 #     breaking=breakingnews.objects.all()
