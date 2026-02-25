4459b7588f3776be29a92a443ab7e0af8b6fc66c
Am <code.c.909.c@gmail.com>
2026-02-26 01:04:26 +0530

    article endless rendering.

--- article/views.py
+++ article/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Herosection,breakingnews,homepagearticle,homepagevideo
 
@@ -5,12 +6,24 @@ from .models import Herosection,breakingnews,homepagearticle,homepagevideo
 def homepage(request):
     hero=Herosection.objects.all()
     breaking=breakingnews.objects.all()
-    articles=homepagearticle.objects.all()
+   # articles=homepagearticle.objects.all()
     videos=homepagevideo.objects.all()
-    return render(request,'article/newslist.html',{"hero":hero,"breaking":breaking,"articles":articles,"videos":videos})
+    article_list = homepagearticle.objects.all()
+    paginator = Paginator(article_list, 8)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'article/newslist.html',{"hero":hero,"breaking":breaking,"page_obj":page_obj,"videos":videos})
 
 def temp(request):
     return render(request,"article/article.html")
 # def breakingnews_text(request):
 #     breaking=breakingnews.objects.all()
 #     return render(request,'article/newscard.html',{"breaking":breaking})
+
+def listing(request):
+    article_list=homepagearticle.objects.all()
+    paginator=Paginator(article_list,2)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+    return render(request,'article/newslist.html',{'page_obj':page_obj})
