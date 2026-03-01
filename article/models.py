85941a0ee2b7316b2478507fb1f8cf8c25bd73db
Am <code.c.909.c@gmail.com>
2026-03-01 19:00:33 +0530

    migration folder fix.

--- article/models.py
+++ article/models.py
@@ -6,18 +6,18 @@ class Herosection(models.Model):
     title=models.CharField(max_length=100)
     description=models.TextField()
     image=models.ImageField(upload_to='Heropage')
-    # slug=models.SlugField(blank=True)
+    slug=models.SlugField(blank=True)
     
-    # def save(self,*args,**kwargs):
-    #     if not self.slug:
-    #         base_slug=slugify(self.title)
-    #         slug=base_slug
-    #         counter=1
-    #         while Herosection.objects.filter(slug=slug).exists():
-    #             slug=f"{base_slug}-{counter}"
-    #             counter+=1
-    #         self.slug=slug
-    #     super().save(*args,**kwargs)
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            base_slug=slugify(self.title)
+            slug=base_slug
+            counter=1
+            while Herosection.objects.filter(slug=slug).exists():
+                slug=f"{base_slug}-{counter}"
+                counter+=1
+            self.slug=slug
+        super().save(*args,**kwargs)
 
     def __str__(self):
         return self.title
