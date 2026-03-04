5e3151cd0c502723a1c3e3ed94a3393ab4b12860
Am <code.c.909.c@gmail.com>
2026-03-04 18:46:09 +0530

    admin panel change

--- article/admin.py
+++ article/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import Herosection,breakingnews,homepagearticle,contactus,authorpage,Comment,Carrer
+from django.contrib import admin
 
+admin.site.site_header = "Ganjam Today Admin"
+admin.site.site_title = "HELLO"
+admin.site.index_title = "Manage Articles & Videos of Ganjam Today"
 # Register your models here.
 admin.site.register(Herosection)
 admin.site.register(breakingnews)
 admin.site.register(homepagearticle)
 admin.site.register(contactus)
 admin.site.register(authorpage)
-admin.site.register(Comment)
 admin.site.register(Carrer)
